cbfcf7bd540bbd5e6749c6333adc1287773fbbe7
Alex Swan <alex@bold-it.com>
2018-10-28 22:16:31 -0400

    slidey tiles

--- game.js
+++ game.js
@@ -3,8 +3,9 @@ var sprites = []
 var tiles = [
     '1','2','3',
     '4','5','6',
-    '7','8',' '
+    '7','8',''
 ]
+var emptyTile = null
 
 // Constants
 const PUZZLE_POS_X = 50
@@ -22,16 +23,31 @@ function damp (a, b, lambda, dt) {
     return lerp(a, b, 1 - Math.exp(-lambda * dt))
 }
 
+function clamp (val, a, b) {
+    if (a < b) {
+        return Math.max(a, Math.min(b, val)) 
+    } else {
+        return Math.max(b, Math.min(a, val)) 
+    }
+}
+
 function tileCanMoveToPosition(tileIndex, newIndex) {
-    if (tiles[newIndex] !== ' ') return false // not open
+    console.log('tileCanMoveToPosition', tileIndex, newIndex)
+    if (tiles[newIndex] !== '') return false // not open
     if (newIndex === tileIndex) return false // same position
     if (tileIndex === newIndex - 3) return true // above
     if (tileIndex === newIndex + 3) return true // below
-    if (Mathfloor(tileIndex / 3) === Math.floor(newIndex / 3)) {
+    if (Math.floor(tileIndex / 3) === Math.floor(newIndex / 3)) {
         if (tileIndex - 1 === newIndex) return true // left
-        if (tileInex + 1 === newIndex) return true // right
+        if (tileIndex + 1 === newIndex) return true // right
     }
     return false
+}
+
+function swapTiles(tileIndex, newIndex) {
+    var temp = tiles[newIndex]
+    tiles[newIndex] = tiles[tileIndex]
+    tiles[tileIndex] = temp
 }
 
 var tile = {
@@ -40,46 +56,76 @@ var tile = {
     width: TILE_SIZE,
     height:TILE_SIZE,
     color: COLOR_GREEN,
+    onDown: function() {
+        if (!tileCanMoveToPosition(this.index, emptyTile.index)) return
+        this.selected = true
+        this.lastPosition = {
+            x: kontra.pointer.x,
+            y: kontra.pointer.y
+        }
+    },
+    onUp: function() {
+        this.selected = false;
+        // If
+    },
     update: function (dt) {
-        // While not touching, damp to its correct position
-        this.desiredX = PUZZLE_POS_X + (this.index % 3) * TILE_SIZE
-        this.desiredY = PUZZLE_POS_Y + Math.floor(this.index / 3) * TILE_SIZE
-        this.x = damp(this.x, this.desiredX, 8, dt)
-        this.y = damp(this.y, this.desiredY, 8, dt)
+        if (this.selected && !kontra.pointer.pressed('left')) {
+            this.onUp()
+        }
+        if (!this.selected) { // While not touching, damp to its correct position
+            this.desiredX = PUZZLE_POS_X + (this.index % 3) * TILE_SIZE
+            this.desiredY = PUZZLE_POS_Y + Math.floor(this.index / 3) * TILE_SIZE
+            this.x = damp(this.x, this.desiredX, 8, dt)
+            this.y = damp(this.y, this.desiredY, 8, dt)
+        } else { // If it's touching, only move along the path it can slide
+            let dx = kontra.pointer.x - this.lastPosition.x
+            let dy = kontra.pointer.y - this.lastPosition.y
+
+            this.x += dx
+            this.y += dy
+
+            // Clamp to the line segment between this and the empty face
+            this.x = clamp(this.x, this.desiredX, emptyTile.desiredX)
+            this.y = clamp(this.y, this.desiredY, emptyTile.desiredY)
+
+            this.lastPosition = {
+                x: kontra.pointer.x,
+                y: kontra.pointer.y
+            }
+        }
     },
     render: function (dt) {
+        if (!this.face) return; // The empty one doesn't get rendered
         kontra.context.save()
+        kontra.context.fillStyle = 'black'
+        kontra.context.fillRect(this.x, this.y, this.width, this.height)
         kontra.context.strokeStyle = this.color
-        kontra.context.fillStyle = this.color
         this.context.font = "96px Courier New"
         this.context.textBaseline = 'top'
         kontra.context.strokeRect(this.x, this.y, this.width, this.height)
+        kontra.context.fillStyle = this.color
         kontra.context.fillText(this.face, this.x, this.y)
         kontra.context.restore()
     }
 }
 
-let ui = kontra.sprite({
-    render: function (dt) {
-        // fill a background
-    }
-})
-
 // Set up the GUI
 let reset = function() {
-    // Create 8 tiles
-    for (let i = 0; i < 8; i++) {
+    // Create 9 tiles
+    for (let i = 0; i < 9; i++) {
         let s = kontra.sprite(tile)
         s.index = i
-        s.face = '' + (i+1)
+        s.face = tiles[i]
         // Put it in its spot
         s.update()
         s.x = s.desiredX
         s.y = s.desiredY
 
-        // Make it touchable
 
         this.sprites.push(s)
+        // Make it touchable
+        if (s.face) kontra.pointer.track(s)
+        else emptyTile = s // This is a special tile
     }
     // OLD CODE
     // // Place the critter in the middle
@@ -107,6 +153,10 @@ var loop = kontra.gameLoop({
         sprites = sprites.filter(sprite => sprite.isAlive());
     },
     render() {
+        // Black background
+        // kontra.context.fillStyle = 'black'
+        kontra.context.fillRect(0,0,kontra.canvas.width, kontra.canvas.height)
+
         sprites.forEach(sprite => sprite.render())
     }
 });
